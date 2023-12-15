7ac14a8ae4ad0168150ed152419036e185ba2574
ImmortalIdiot <max.krasulya@yandex.ru>
2023-12-15 23:48:31 +0300

    Fixed the displaying of Settings fragment; fixed the codestyle

--- app/src/main/java/com/immortalidiot/studentapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/immortalidiot/studentapp/ui/settings/SettingsFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.immortalidiot.studentapp.databinding.FragmentSettingsBinding;
 
@@ -17,15 +15,9 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savesInstanceState) {
 
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
+        FragmentSettingsBinding binding = FragmentSettingsBinding
+                .inflate(inflater, container, false);
 
-        com.immortalidiot.studentapp.databinding.FragmentSettingsBinding binding =
-                FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 }
